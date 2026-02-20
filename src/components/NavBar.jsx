4faa178536f496aed2b0d94be36a95ceab72f7a4
suryaa20 <152994046+suryaa20@users.noreply.github.com>
2026-02-20 09:42:21 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import ThemeToggle from './ThemeToggle';
-import './Navbar.css';
+import './NavBar.css';
 
 const Navbar = () => {
   const navigate = useNavigate();
